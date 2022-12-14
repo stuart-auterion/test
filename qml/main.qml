9df315ed76326562fcd32f0e89720c2bc624022e
Stuart Miller <stuart@auterion.com>
2022-12-14 15:09:18 -0600

    Initial commit.

---
+++ qml/main.qml
@@ -0,0 +1,16 @@
+import QtQuick 2.15
+import QtQuick.Layouts 1.15
+import QtQuick.Window 2.15
+import QtQuick.Controls 2.15
+import Qt.labs.animation 1.0
+import QtQml.Models 2.15
+import QtGraphicalEffects 1.15
+//import Video 1.0
+
+Window {
+    id: root
+    width: 810
+    height: 480
+    visible: true
+    title: qsTr("Hello World")
+}
---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+#include <QGuiApplication>
+#include <QObject>
+#include <QQmlApplicationEngine>
+#include <QQmlContext>
+#include <QQmlEngine>
+
+int main(int argc, char* argv[]) {
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QGuiApplication app(argc, argv);
+
+    QQmlApplicationEngine engine;
+    const QUrl url(QStringLiteral("qrc:/qml/main.qml"));
+    engine.addImportPath("qrc:/src/");
+    QObject::connect(
+        &engine, &QQmlApplicationEngine::objectCreated, &app,
+        [url](QObject* obj, const QUrl& objUrl) {
+            if (!obj && url == objUrl) {
+                QCoreApplication::exit(-1);
+            }
+        },
+        Qt::QueuedConnection);
+
+    engine.load(url);
+
+    return app.exec();
+}
